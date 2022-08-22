1cbadcd3f2425e925b3198a312772cc5b109e305
heavy-syzygy <88018628+heavy-syzygy@users.noreply.github.com>
2022-08-22 21:20:45 +0800

    generate mock data for realtime database

---
+++ backend/seed_data.py
@@ -0,0 +1,33 @@
+import firebase_admin, os
+#import pandas as pd
+import json
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate("C:/Users/shawk/Downloads/CZ3002-ASE/project/data/ase-2022-732e9-firebase-adminsdk-2wzrp-58363f63f6.json")
+firebase_admin.initialize_app(cred,
+{'databaseURL': 'https://ase-2022-732e9-default-rtdb.asia-southeast1.firebasedatabase.app/'})
+
+ref = db.reference("/")
+course_list = ["CZ3002", "CZ4045", "CZ3006", "CZ1012", "CZ4067", "CZ3005"]
+for course in course_list:
+    ref = db.reference("/Forum/"+course)
+
+    with open("forum.json", "r") as f:
+            file_contents = json.load(f)
+            
+    #for key, value in file_contents.items():
+    #    ref.push().set(value)
+    ref.set(file_contents)
+
+    ref = db.reference("/IndexSwap/"+course)
+    with open("swapIndex.json", "r") as f:
+        file_contents = json.load(f)
+    ref.set(file_contents)
+
+ref = db.reference("/TeamFind")
+with open("findTeam.json", "r") as f:
+    file_contents = json.load(f)
+ref.set(file_contents)
+
+
+
