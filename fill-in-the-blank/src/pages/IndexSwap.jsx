311868df9f701ff4bcf16d30c8db2454dbac8d4d
dannyyys <dannyyys@users.noreply.github.com>
2022-08-27 14:07:18 +0800

    ci: :arrow_up: Add firebase integration

--- fill-in-the-blank/src/pages/IndexSwap.jsx
+++ fill-in-the-blank/src/pages/IndexSwap.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { db } from "../firebase";
+import { collection, getDocs, where, query } from "firebase/firestore";
+
 import styles from "./IndexSwap.module.css";
 
 const IndexSwap = () => {
+  const q = query(collection(db, "Forum"), where("Comment", "==", "Hi"));
+
+  async function getData() {
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+  getData();
+
   return (
     <div className={styles.indexSwap}>
       <h1>Index swap page</h1>
