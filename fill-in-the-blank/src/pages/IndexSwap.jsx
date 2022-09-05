e52f46188b88c9a0445bfdb98800b80342f9a2e9
heavy-syzygy <88018628+heavy-syzygy@users.noreply.github.com>
2022-09-05 14:42:33 +0800

    feat: Add Index Swap features

--- fill-in-the-blank/src/pages/IndexSwap.jsx
+++ fill-in-the-blank/src/pages/IndexSwap.jsx
@@ -1,27 +1,143 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { db } from "../firebase";
 import { collection, getDocs, where, query } from "firebase/firestore";
 
 import styles from "../styles/pages/IndexSwap.module.css";
+import IndexSearchBars from "../components/IndexSearchBars";
+import IndexSwapTable from "../components/IndexSwapTable";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { firebaseAuth } from "../firebase";
+
 
 const IndexSwap = () => {
-  const q = query(
+  const [user, loading, error] = useAuthState(firebaseAuth);
+
+  const [moduleData, setModuleData] = useState([]);
+  const [searchModuleCode, setSearchModuleCode] = useState("");
+  const [searchCurrentIndex, setSearchCurrentIndex] = useState("");
+  const [searchDesiredIndex, setSearchDesiredIndex] = useState("");
+  const [searchState, setSearchState] = useState(false);
+
+  const navigate = useNavigate();
+
+  const searchQuery = query(
     collection(db, "Index-Swap"),
-    where("Email", "==", "xyz@gmail.com")
+    where("Email", "==", user.email)
   );
 
-  async function getData() {
-    const querySnapshot = await getDocs(q);
+  async function getIndexSwapData() {
+    let result = [];
+    const querySnapshot = await getDocs(searchQuery);
 
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
+      result.push(doc.data());
     });
-  }
-  getData();
+    setModuleData(result);
+  };
+
+  useEffect(() => {
+    getIndexSwapData();
+  }, [searchState]);
+
+  const clickSearchButton = (searchModuleCode,searchCurrentIndex,searchDesiredIndex) => {
+    setSearchState(!searchState);
+    async function validateCourseCode() {
+      let result = [];
+      const courseCodeSearchQuery = query(
+        collection(db, "Module"),
+        where("Module Code", "==", searchModuleCode)
+      );
+      const querySnapshot = await getDocs(courseCodeSearchQuery);
+
+      querySnapshot.forEach((doc) => {
+        result.push(doc.data());
+      });
+
+      return result.items;
+    };
+
+    async function validateCurrentIndexNum() {
+      let result = [];
+      const currentIndexNumSearchQuery = query(
+        collection(db, "Module"),
+        where("Module Code", "==", searchModuleCode),
+        where("Index", "array-contains", searchCurrentIndex)
+      );
+      const querySnapshot = await getDocs(currentIndexNumSearchQuery);
+
+      querySnapshot.forEach((doc) => {
+        result.push(doc.data());
+      });
+
+      return result.items;
+    };
+
+    async function validateDesiredIndexNum() {
+      let result = [];
+      const desiredIndexNumSearchQuery = query(
+        collection(db, "Module"),
+        where("Module Code", "==", searchModuleCode),
+        where("Index", "array-contains", searchDesiredIndex)
+      );
+      const querySnapshot = await getDocs(desiredIndexNumSearchQuery);
+
+      querySnapshot.forEach((doc) => {
+        result.push(doc.data());
+      });
+
+      return result.items;
+    };
+
+    if (searchModuleCode === "" || searchCurrentIndex==="" || searchDesiredIndex===""){
+        window.alert("Please fill up all search fields!");
+    }
+    else if (validateCourseCode.length === 0){
+        window.alert("Invalid Course Code!" +validateCourseCode.length+"  1")
+    }
+    else if (validateCurrentIndexNum.length === 0){
+        window.alert("Invalid Index Number!")
+    }
+    else if (validateDesiredIndexNum.length === 0){
+        window.alert("Invalid Index Number!")
+    }
+    else if (searchCurrentIndex===searchDesiredIndex){
+        window.alert("You have already obtained the index that you want!")
+    }
+    else {
+        navigate(`/indexSwap/${searchModuleCode}/${searchCurrentIndex}/${searchDesiredIndex}`);
+    }
+    console.log(searchModuleCode+";"+searchCurrentIndex+";"+searchDesiredIndex);
+  };
 
   return (
     <div className={styles.indexSwap}>
-      <h1>Index swap page</h1>
+      <h1 className={styles.indexSwapHeader}>Index Swap</h1>
+
+      <h2 className={styles.indexSwapHeader}>Your Requests</h2>
+      <div className={styles.indexSwapBody}>
+        <div className={styles.handleOverflow}>
+          <IndexSwapTable moduleData={moduleData} />
+          </div>
+      </div>
+
+      <p />
+
+      <h2 className={styles.indexSwapHeader}>Search For Peer Requests</h2>
+      <div className={styles.indexSwapBody}>
+        <IndexSearchBars
+          searchModuleCode={searchModuleCode.toUpperCase()}
+          setSearchModuleCode={setSearchModuleCode}
+          searchCurrentIndex={searchCurrentIndex}
+          setSearchCurrentIndex={setSearchCurrentIndex}
+          searchDesiredIndex={searchDesiredIndex}
+          setSearchDesiredIndex={setSearchDesiredIndex}
+          clickSearchButton={clickSearchButton}
+        />
+      </div>
+
     </div>
   );
 };
---
+++ fill-in-the-blank/src/pages/IndexSwapSearch.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { db } from "../firebase";
+import { collection, getDocs, where, query } from "firebase/firestore";
+
+import styles from "../styles/pages/IndexSwapSearch.module.css";
+import IndexSwapSearchTable from "../components/IndexSwapSearchTable";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { firebaseAuth } from "../firebase";
+
+
+const IndexSwapSearch = () => {
+  const [user, loading, error] = useAuthState(firebaseAuth);
+
+  const [moduleData, setModuleData] = useState([]);
+  const { moduleCode, currentIndex, desiredIndex } = useParams();
+
+  const searchQuery = query(
+    collection(db, "Index-Swap"),
+    where("Email", "!=", user.email),
+    where("Module Code", "==", moduleCode),
+    where("IndexObtained", "==", currentIndex),
+    where("IndexWanted", "==", desiredIndex)
+  );
+
+  async function getIndexSwapData() {
+    let result = [];
+    const querySnapshot = await getDocs(searchQuery);
+
+    querySnapshot.forEach((doc) => {
+      result.push(doc.data());
+    });
+    setModuleData(result);
+  }
+
+  useEffect(() => {
+      //getIndexSwapData();
+  });
+
+  return (
+    <div className={styles.indexSwap}>
+      <h1 className={styles.indexSwapHeader}>Index Swap</h1>
+
+      <h2 className={styles.indexSwapHeader}>Your Requests</h2>
+      <div className={styles.indexSwapBody}>
+        <div className={styles.handleOverflow}>
+          <IndexSwapSearchTable moduleData={moduleData} />
+          </div>
+      </div>
+
+      <p />
+
+      <h2 className={styles.indexSwapHeader}>Insert Create Request</h2>
+      <div className={styles.indexSwapBody}>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default IndexSwapSearch;
