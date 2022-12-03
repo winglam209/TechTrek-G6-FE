a81a9ae7564ba1afe8a7b83d3862507a1b580924
K-156 <limstephanie156@gmail.com>
2022-12-03 15:03:11 +0800

    create auth function

---
+++ fill-in-the-blank/src/services/auth.js
@@ -0,0 +1,11 @@
+const addUserToLocalStorage = ({ user, token }) => {
+  localStorage.setItem("user", JSON.stringify(user));
+  localStorage.setItem("token", token);
+};
+
+const removeUserFromLocalStorage = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+};
+
+export { addUserToLocalStorage, removeUserFromLocalStorage };
