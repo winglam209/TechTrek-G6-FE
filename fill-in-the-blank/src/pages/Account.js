4022395d237474d585dda0d084d11d345c8c23f6
Wing Lam Liu <winglamliu@Wings-MacBook-Air.local>
2022-12-03 14:34:57 +0800

    added account table

--- fill-in-the-blank/src/pages/Account.js
+++ fill-in-the-blank/src/pages/Account.js
@@ -2,16 +2,31 @@ import styled from "styled-components";
 import { useEffect } from "react";
 import { useAppContext } from "../context/appContext";
 import { StatsContainer } from "../components";
+import Table from "../components/Table"
 
 const Account = () => {
   const { showStats, isLoading, monthlyTransactions } = useAppContext();
-  //   useEffect(() => {
-  //     showStats();
-  //   }, []);
 
-  // if (isLoading) {
-  //   return <Loading center />;
-  // }
+  const cols = [
+    { field: 'AccountID', headerName: 'Account ID', width: 200 },
+    { field: 'AccountType', headerName: 'Receiving Account ID', width: 200 },
+    { field: 'AcccountBalance', headerName: 'Date', width: 200 }
+];
+
+  const rows = [
+    {
+        "AccountID": 621156213,
+        "UserID": 1,
+        "AccountType": "Saving",
+        "AcccountBalance": 70200.71
+    },
+    {
+        "AccountID": 958945214,
+        "UserID": 1,
+        "AccountType": "Current",
+        "AcccountBalance": 99720.46
+    },
+  ];
 
   return (
     <>
@@ -21,13 +36,17 @@ const Account = () => {
           <p>Let's work together to keep your finances in check!</p>
         </section>
         <StatsContainer />
+        <Table
+          rows={rows}
+          cols={cols}
+        />
       </Wrapper>
     </>
   );
 };
 
 const Wrapper = styled.main`
-  padding: 3rem;
+   padding: 3rem;
   .chart-overview {
     margin: 0 auto;
     width: 300px;
